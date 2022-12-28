16ce4f0eba1cafa1faa3bfa0c7a49bd4dea2bfa6
DevMazed04 <mdmazedislam2684@gmail.com>
2022-12-28 21:08:49 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
+import Navbar from "./Pages/Navbar/Navbar";
 
 function App() {
   return (
     <div className="">
-      <button
-        type="button"
-        class="inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs uppercase rounded-full shadow-md hover:bg-red-700">
-        Danger
-      </button>
+      <Navbar></Navbar>
+      <br /> <br />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,13 @@ module.exports = {
   content: [
     "./src/**/*.{html,js,jsx,ts,tsx}",
     "./node_modules/tw-elements/dist/js/**/*.js",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('tw-elements/dist/plugin')
+    require('tw-elements/dist/plugin'),
+    require('flowbite/plugin')
   ]
 };
